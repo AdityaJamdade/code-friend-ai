3d11122c808750623131dea44b1af1c1e0b80bc5
AdityaJamdade <adijamdade156@gmail.com>
2023-12-21 14:15:33 +0530

    navbar 1.0 done

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+// Navbar.jsx
+import './Navbar.css'; // Import the CSS file for styling
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <a href="#" className="logo">COODEFRIEND</a>
+            <a href="#" className="about-link">About</a>
+        </div>
+    );
+}
+
+export default Navbar;
