f968a62a87d175fe9e1489f5471438fa9cb6c104
AdityaJamdade <adijamdade156@gmail.com>
2024-01-02 10:02:38 +0530

    navbar styling change

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,17 @@ import './Navbar.css';
 const Navbar = () => {
     return (
         <div className="navbar">
-            <a href="/" className="logo">CODE - FRIEND</a>
-            <a href="/about" className="about-link">About</a>
-            <a href="/login" className="about-link">Login</a>
+            <span>
+                <a href="/" className="logo">CODE - FRIEND </a>
+            </span>
+            <ul>
+                <li className="nav-item">John Doe</li>
+                <li className="nav-item">Logout</li>
+                <li className="nav-item">
+                    <a href="/about" className="about-link">About</a>
+                </li>
+            </ul>
+
         </div>
     );
 }
