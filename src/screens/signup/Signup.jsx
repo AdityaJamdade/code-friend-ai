c4ae4267aa502275eaea8257fa62a64b858fcda5
AdityaJamdade <adijamdade156@gmail.com>
2024-01-03 15:11:19 +0530

    added signup styles

--- src/screens/signup/Signup.jsx
+++ src/screens/signup/Signup.jsx
@@ -1,13 +1,15 @@
-import './Signup.css'
+import './Signup.css';
 
 const Signup = () => {
   return (
-    <div className='signup-box'>
-        <button className="sign-in-with-git">Sign Up with Github</button>
-        
-        
+    <div className='signup-container'>
+      <div className='signup-card'>
+        <h2>Sign Up</h2>
+        <button className="sign-in-button google">Sign Up with Google</button>
+        <button className="sign-in-button github">Sign Up with GitHub</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
