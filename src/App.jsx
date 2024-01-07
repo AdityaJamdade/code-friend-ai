15690a04e176885f3afcac7b17e5baad86da3d0c
AdityaJamdade <adijamdade156@gmail.com>
2024-01-07 11:17:34 +0530

    login success for google and github

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Signup from './screens/signup/Signup'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const getUser = () => {
+      fetch("http://localhost:8000/auth/login/success", {
+        method: 'GET',
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+          "Access-Control-Allow-Credentials": true,
+        },
+      }).then((response) => {
+        if (response.status === 200) return response.json();
+        throw new Error("Authentication has been failed");
+      }).then(resObj => {
+        setUser(resObj.user)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+
+    getUser();
+  }, [])
+  
+  console.log(user);
 
   return (
     <>
